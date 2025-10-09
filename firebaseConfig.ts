ba735c35a8b32292bc5eb3d8497f6ca15d435d1e
Brosaidon <pandawab@me.com>
2025-10-09 11:34:56 +0200

    🔥 firebaseConfig created

---
+++ firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6YRQ6KFl-EmoxWZUMLPB0us4YXteEGNw",
+  authDomain: "d4estation-bef35.firebaseapp.com",
+  databaseURL: "https://d4estation-bef35-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "d4estation-bef35",
+  storageBucket: "d4estation-bef35.firebasestorage.app",
+  messagingSenderId: "929478232053",
+  appId: "1:929478232053:web:8a714255ddd84ed78fc2bc",
+  measurementId: "G-W7Q9L9KPWW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
