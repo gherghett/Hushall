08ac6fdd093a9e1e2f332a4746eb66f5ceede9c6
ratmastererik <erik.lindgrenw@gmail.com>
2025-10-17 11:53:28 +0200

    added CreateHousehold component which uploads new household to firebase

---
+++ components/CreateHousehold.tsx
@@ -0,0 +1,44 @@
+import { userAtom } from "@/atoms/auth-atoms";
+import { db } from "@/lib/firebase";
+import { AppTheme } from "@/lib/theme";
+import { addDoc, collection } from "firebase/firestore";
+import { useAtomValue } from "jotai";
+import React, { useState } from "react";
+import { Button, Card, Text, TextInput, useTheme } from "react-native-paper";
+
+export default function CreateHoushold() {
+  const theme = useTheme() as AppTheme;
+  const [householdName, setHouseholdName] = useState("");
+  const user = useAtomValue(userAtom);
+
+  const handleCreateHouseholdSubmit = async () => {
+    console.log(householdName);
+
+    try {
+      const ref = await addDoc(collection(db, "household"), {
+        name: { householdName },
+        code: 123,
+        application: [],
+        members: [],
+        chores: [],
+      });
+    } catch (error) {
+      console.error("Error creating household:", error);
+    }
+  };
+
+  return (
+    <Card>
+      <Text variant="titleLarge">Skapa Hushåll</Text>
+      <Text variant="labelMedium">Hushållsnamn</Text>
+      <TextInput
+        label={"namn"}
+        onChangeText={setHouseholdName}
+        mode="outlined"
+      />
+      <Button mode="contained" onPress={handleCreateHouseholdSubmit}>
+        Create
+      </Button>
+    </Card>
+  );
+}
