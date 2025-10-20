834238eab771f26020914ff1e93e23ed8a5e4dd8
ratmastererik <erik.lindgrenw@gmail.com>
2025-10-20 11:28:35 +0200

    added function to generate invitation code

---
+++ lib/createInviteCode.ts
@@ -0,0 +1,28 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "./firebase";
+
+const CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+const generateRandomCode = (lenght = 6) => {
+  let code = "";
+  for (let i = 0; i < lenght; i++) {
+    code += CHAR_SET.charAt(Math.floor(Math.random() * CHAR_SET.length));
+  }
+  return code;
+};
+
+export async function generateUniqueJoinCode() {
+  let code;
+  let exists = true;
+
+  while (exists) {
+    code = generateRandomCode();
+
+    const q = query(collection(db, "Households"), where("code", "==", code));
+    const querySnap = await getDocs(q);
+
+    exists = !querySnap.empty;
+  }
+
+  return code;
+}
