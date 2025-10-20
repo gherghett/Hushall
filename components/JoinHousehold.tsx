c749386650c5db09480ca255d2c77761ba9e310c
ratmastererik <erik.lindgrenw@gmail.com>
2025-10-20 21:33:32 +0200

    added functionality to HandleJoinHousehold

--- components/JoinHousehold.tsx
+++ components/JoinHousehold.tsx
@@ -1,7 +1,17 @@
 import { userAtom } from "@/atoms/auth-atoms";
+import { db } from "@/lib/firebase";
 import { AppTheme } from "@/lib/theme";
+import {
+  arrayUnion,
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { useAtomValue } from "jotai";
 import React, { useState } from "react";
+import { Alert } from "react-native";
 import {
   Button,
   Card,
@@ -11,15 +21,38 @@ import {
   useTheme,
 } from "react-native-paper";
 
-export default function CreateHoushold() {
+export default function JoinHoushold() {
   const theme = useTheme() as AppTheme;
   const user = useAtomValue(userAtom);
   const [householdCode, setHouseholdCode] = useState("");
   const [modalVisible, setModalVisible] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleJoinHouseholdSubmit = async () => {
+  const handleJoinHouseholdSubmit = async (code: string) => {
     setIsLoading(true);
+    try {
+      const q = query(collection(db, "Households"), where("code", "==", code));
+      const querySnap = await getDocs(q);
+
+      if (querySnap.empty) {
+        Alert.alert("Fel", "Ingen hushåll hittades med den koden.");
+        setIsLoading(false);
+        return;
+      }
+
+      const householdDoc = querySnap.docs[0];
+
+      await updateDoc(householdDoc.ref, {
+        members: arrayUnion({
+          id: user!.uid,
+          name: user!.displayName,
+        }),
+      });
+    } catch (error) {
+      console.error("Error joining household:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -38,7 +71,7 @@ export default function CreateHoushold() {
             disabled={isLoading}
             onPress={() => {
               setModalVisible(!modalVisible);
-              handleJoinHouseholdSubmit();
+              handleJoinHouseholdSubmit(householdCode);
             }}
           >
             Join
