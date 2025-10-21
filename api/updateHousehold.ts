ee4a12a6fc23cd4eaab3c4142d3dbc6ec9bb3087
ratmastererik <erik.lindgrenw@gmail.com>
2025-10-21 13:33:18 +0200

    implement joinHousehold function to allow users to join a household by code

--- api/updateHousehold.ts
+++ api/updateHousehold.ts
@@ -1,1 +1,47 @@
-elkasdjlksajd;
+import { db } from "@/lib/firebase";
+import {
+  arrayUnion,
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+
+interface JoinHouseholdProps {
+  code: string;
+  userId: string;
+  userName: string;
+}
+
+export default async function joinHousehold({
+  code,
+  userId,
+  userName,
+}: JoinHouseholdProps): Promise<{ success: boolean; message: string }> {
+  try {
+    const q = query(collection(db, "households"), where("code", "==", code));
+    const querySnap = await getDocs(q);
+
+    if ((await querySnap).empty) {
+      return {
+        success: false,
+        message: "Ingen hushåll hittades med den koden",
+      };
+    }
+
+    const householdDoc = querySnap.docs[0];
+
+    await updateDoc(householdDoc.ref, {
+      members: arrayUnion({
+        uid: userId,
+        name: userName,
+      }),
+    });
+
+    return { success: true, message: "Du har gått med i hushållet!" };
+  } catch (error) {
+    console.error("Error joining household:", error);
+    return { success: false, message: "Något gick fel. Försök igen." };
+  }
+}
