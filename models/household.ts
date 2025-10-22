1df7344bea00cf4790b71094e054511c3888aeea
Daniel Gherghetta <gherghetta@gmail.com>
2025-10-22 11:47:14 +0200

    model update, new seedData to fit with read users

--- models/household.ts
+++ models/household.ts
@@ -4,16 +4,16 @@ export const memberSchema = z.object({
   id: z.string(),
   userId: z.string().optional(), //potentiellProblem
   name: z.string().min(1, "Name is required"), //potentiellProblem
-  characterId: z.enum(["0", "1", "2", "3", "4", "5", "6", "7", "8"]),
+  characterId: z.number().int().min(1).max(9),
   role: z.enum(["owner", "member"]),
 });
 
 export type Member = z.infer<typeof memberSchema>;
 
-export const completionSchema = z.object ({
-    completedBy: z.array(z.string()).default ([]), //memberId
-    completedAt: z.string(),
-})
+export const completionSchema = z.object({
+  completedBy: z.array(memberSchema).default([]),
+  completedAt: z.string(),
+});
 
 export const choreSchema = z.object({
   id: z.string(),
@@ -24,11 +24,32 @@ export const choreSchema = z.object({
   completions: z.array(completionSchema).default([]),
 });
 
-export const householdSchema = z.object ({
-    id: z.string(),
-    name: z.string(),
-    code: z.string(),
-    chores: z.array(choreSchema),
-    applications: z.array(memberSchema).default([]),
-    members: z.array(memberSchema).default([]),
-})
+export type Chore = z.infer<typeof choreSchema>;
+
+export const createChoreFormSchema = choreSchema.omit({
+  id: true,
+  completions: true,
+});
+
+export type CreateChore = z.infer<typeof createChoreFormSchema>;
+
+export const householdSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  code: z.string(),
+  applications: z.array(z.string()).default([]),
+  members: z.array(memberSchema).default([]),
+  chores: z.array(choreSchema).default([]),
+});
+
+export type Household = z.infer<typeof householdSchema>;
+
+export const createHouseholdFormSchema = householdSchema.omit({
+  id: true,
+  code: true,
+  applications: true,
+  members: true,
+  chores: true,
+});
+
+export type CreateHousehold = z.infer<typeof createHouseholdFormSchema>;
