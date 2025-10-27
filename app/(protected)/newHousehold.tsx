542bc12a1f0184ef46e7dc93c574e0ebb7d425a2
ratmastererik <erik.lindgrenw@gmail.com>
2025-10-27 11:43:29 +0100

    format and remove unused screens

--- app/(protected)/newHousehold.tsx
+++ app/(protected)/newHousehold.tsx
@@ -1,13 +1,10 @@
 import { AppTheme } from "@/lib/theme";
 import { View } from "react-native";
 import { useTheme } from "react-native-paper";
-import CreateHousehold from "./createHousehold";
-import JoinHousehold from "./joinHousehold";
-
+import CreateHousehold from "../../components/CreateHousehold";
+import JoinHousehold from "../../components/JoinHousehold";
 export default function NewHouseholdScreen() {
   const theme = useTheme() as AppTheme;
-
-  console.log("ny sida");
 
   return (
     <View style={theme.styles.container}>
