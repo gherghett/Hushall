35528bb3dec363f34ee77797706f01ef27baedf9
Eliino <none>
2025-10-27 15:30:15 +0100

    Adding theme to see how it looks

--- app/(protected)/settings.tsx
+++ app/(protected)/settings.tsx
@@ -1,23 +1,27 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { useCharacters } from "@/hooks/useCharacters";
 import { AppTheme } from "@/lib/theme";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Divider, List, Surface, Text, useTheme } from "react-native-paper";
+
 
 export default function SettingsScreen() {
   const theme = useTheme() as AppTheme;
+  const characters = useCharacters();
 
   return (
     <View style={[theme.styles.container]}>
       <Surface>
         <Text> Inställningar</Text>
-        <Text> Profil</Text>
+        <View>
+          <Text style={theme.styles.title}> Profil</Text>
+        </View>
         <View>
           <Text>Rund cirkel med karaktären användaren valt/blivit tilldelad</Text>
           <Text>Input field där namnet på användaren står, ska gå att ändra</Text>
         </View>
         <View>
-          <Text>Hushåll</Text>
+          <Text style={theme.styles.title}>Hushåll</Text>
         </View>
         <View>
           <Text>Input field namn på hushåll, går att ändra</Text>
@@ -31,7 +35,7 @@ export default function SettingsScreen() {
           <Text>Lista med alla hushållsmedlemmar</Text>
           <Text>Plus knapp för att skapa ny medlem</Text>
         </View>
-        <Text>Global</Text>
+        <Text style={theme.styles.title}>Global</Text>
         <Text>Light/dark/auto mode switch</Text>
         <Divider></Divider>
         <ThemeToggle />
@@ -39,3 +43,14 @@ export default function SettingsScreen() {
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  headerContainer: {
+
+  },
+  textTitle: {
+    textAlign: "center",
+
+  },
+
+})
