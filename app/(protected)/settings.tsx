15fab11ed2e03b34e37952f1318403b64eed5d6a
Daniel Gherghetta <gherghetta@gmail.com>
2025-10-28 11:31:05 +0100

    add test functionality to edit household name in settings

--- app/(protected)/settings.tsx
+++ app/(protected)/settings.tsx
@@ -1,10 +1,25 @@
+import {
+  useCurrentHousehold,
+  useEditHouseholdNameMutation,
+} from "@/atoms/household-atoms";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { AppTheme } from "@/lib/theme";
-import { View } from "react-native";
+import { Button, View } from "react-native";
 import { Divider, Surface, Text, useTheme } from "react-native-paper";
 
 export default function SettingsScreen() {
   const theme = useTheme() as AppTheme;
+  const household = useCurrentHousehold();
+  const editNameMutation = useEditHouseholdNameMutation();
+
+  // här ser du hur man ändrar namnet---- du behöver såklart skicka in det namnet som användaren skrivit in i textruta eller vad det kan bara
+  const handleTestEditName = () => {
+    if (!household) return;
+    editNameMutation.mutate({
+      name: "TestName" + Math.floor(Math.random() * 1000),
+      id: household.id,
+    });
+  };
 
   return (
     <View style={[theme.styles.container]}>
@@ -12,6 +27,7 @@ export default function SettingsScreen() {
         <Text> Settings</Text>
         <Divider></Divider>
         <ThemeToggle />
+        <Button title="Test Edit Household Name" onPress={handleTestEditName} />
       </Surface>
     </View>
   );
