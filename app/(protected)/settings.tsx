63252e038f369f735d4e459f5c90032de26b750c
Brosaidon <pandawab@me.com>
2025-10-30 10:04:06 +0100

    paus commit

--- app/(protected)/settings.tsx
+++ app/(protected)/settings.tsx
@@ -1,28 +1,27 @@
+import { userAtom } from "@/atoms/auth-atoms";
 import {
   useCurrentHousehold,
   useEditHouseholdNameMutation,
 } from "@/atoms/household-atoms";
-import ChooseCharacter from "@/components/ChooseCharacter";
-import { ThemeToggle } from "@/components/ThemeToggle";
-import { Character, useCharacters } from "@/hooks/useCharacters";
+import {
+  useUpdateMemberNameMutation,
+  useUpdateUserCharacterMutation,
+} from "@/atoms/userAtoms";
 import SelectedCharacter from "@/components/SelectedCharacter";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { useCurrentUserCharacterId } from "@/hooks/getCurrentCharacter";
+import { Character, useCharacters } from "@/hooks/useCharacters";
 import { AppTheme } from "@/lib/theme";
+import { useAtomValue } from "jotai";
 import { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import {
   Divider,
   Surface,
   Text,
-  useTheme,
   TextInput,
+  useTheme,
 } from "react-native-paper";
-import { useAtomValue } from "jotai";
-import { userAtom } from "@/atoms/auth-atoms";
-import {
-  useUpdateUserCharacterMutation,
-  useUpdateMemberNameMutation,
-} from "@/atoms/userAtoms";
-import { useCurrentUserCharacterId } from "@/hooks/getCurrentCharacter";
 
 export default function SettingsScreen() {
   const theme = useTheme() as AppTheme;
@@ -85,13 +84,13 @@ export default function SettingsScreen() {
   );
 
   return (
-    <View style={[styles.bodyContainer]}>
-      <Surface>
+    <View>
+      <Surface style={theme.styles.Surface}>
         <View>
-          <Text style={[theme.styles.title, styles.textTitle]}> Profil</Text>
-          <Divider/>
+          <Text variant="displaySmall"> Profil</Text>
+          <Divider />
         </View>
-        <View style={styles.characterRow}>
+        <View>
           <SelectedCharacter
             characters={availableCharacters}
             selectedCharacter={selectedCharacter}
@@ -99,93 +98,38 @@ export default function SettingsScreen() {
           />
 
           <TextInput
-            style={styles.nameInput}
-            value={memberName}
-            onChangeText={setMemberName}
-            onBlur={() => handleMemberNameChange(memberName)}
-            placeholder="Användarnamn"
-            placeholderTextColor="#999"
+            label="Byt namn på medlem."
           />
+        </View>
+      </Surface>
+      <Surface>
+        <View>
+
+          <Divider />
+          <Text variant="displaySmall">Hushåll</Text>
+          <Divider />
         </View>
         <View>
-          <Divider/>
-          <Text style={[theme.styles.title, styles.textTitle]}>Hushåll</Text>
-          <Divider/>
-        </View>
-        <View style={styles.householdRow}>
           <TextInput
-            style={styles.householdInput}
-            value={householdName}
-            onChangeText={setHouseholdName}
-            onBlur={() => handleHouseholdNameChange(householdName)}
-            placeholder="Hushållsnamn"
-            placeholderTextColor="#999"
-          />
+            label="But manm på hushåll." />
         </View>
-        <View style={styles.codeContainer}>
-          <Text style={styles.codeText}>{household?.code || "Ingen kod"}</Text>
+        <View>
+          <Text>{household?.code || "Ingen kod"}</Text>
         </View>
         <View>
           <Text>Lista med användare - VG-krav</Text>
           <Text>Plus knapp för att skapa ny medlem</Text>
         </View>
         <View>
-          <Divider/>
-          <Text style={[theme.styles.title, styles.textTitle]}>Global</Text>
-          <Divider/>
+          <Divider />
+          <Text >Global</Text>
+          <Divider />
         </View>
-        <Divider/>
-        <ThemeToggle /> {/* Reminder: Dark/light/auto switch */}
-        <Divider></Divider>
-      </Surface>
-    </View>
+      </Surface >
+      <Divider />
+      <ThemeToggle /> {/* Reminder: Dark/light/auto switch */}
+      <Divider></Divider>
+
+    </View >
   );
 }
-
-const styles = StyleSheet.create({
-  bodyContainer: {
-    flex: 1,
-  },
-  textTitle: {
-    textAlign: "center",
-    padding: 20,
-    paddingBottom: 0,
-    fontSize: 20,
-  },
-  characterRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingHorizontal: 16,
-    marginVertical: 12,
-  },
-  nameInput: {
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    fontSize: 16,
-    flex: 1,
-    marginLeft: 20,
-  },
-  householdRow: {
-    paddingHorizontal: 16,
-    marginVertical: 12,
-  },
-  householdInput: {
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 12,
-    paddingVertical: 10,
-    fontSize: 16,
-    width: "100%",
-  },
-  codeText: {
-    fontSize: 18,
-    textAlign: "center",
-  },
-  codeContainer: {
-    paddingHorizontal: 16,
-    marginVertical: 12,
-  },
-});
